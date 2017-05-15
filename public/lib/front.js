f1796ad8e915bcd0309b9b9fc44d8c9e52ecdc81
Lucas Montero <lucas.montero.mauriz@gmail.com>
2017-05-15 16:57:24 +0200

    Working in front page

--- public/lib/front.js
+++ public/lib/front.js
@@ -1,1 +1,16 @@
-console.log("front.js");
+$(function () {
+      $(window).scroll(function() {
+	  if ($(this).scrollTop() >= 50) {        
+		  $('#goUp').fadeIn(200);    
+	  } else {
+		  $('#goUp').fadeOut(200);   
+	  }
+   });
+  
+   $('#goUp').click(function() {    
+	 				console.log("dentro");
+					$('body,html').animate({
+						scrollTop : 0                     
+				  }, 500);
+   });
+});

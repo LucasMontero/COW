2d28c523e1b6b1982ba08ddb74065b6f7eb03c16
Lucas Montero <lucas.montero.mauriz@gmail.com>
2017-05-18 12:09:22 +0200

    Working on front page

--- public/lib/front.js
+++ public/lib/front.js
@@ -1,1 +1,16 @@
-//You can use plain javascript, JQuery or angular notation here.
+$(function () {
+      $(window).scroll(function() {
+	  if ($(this).scrollTop() >= 50) {        
+		  $('#goUp').fadeIn(200);    
+	  } else {
+		  $('#goUp').fadeOut(200);   
+	  }
+   });
+  
+   $('#goUp').click(function() {    
+	 				console.log("dentro");
+					$('body,html').animate({
+						scrollTop : 0                     
+				  }, 500);
+   });
+});
